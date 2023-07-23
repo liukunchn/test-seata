1cc1910f010a44b71ba03b28ecabc4704502eef1
liukunchn <liukunchn@outlook.com>
2023-07-23 16:02:18 +0800

    1

---
+++ test-seata-order/src/main/java/com/maoyou/testseata/order/TestSeataOrderApplication.java
@@ -0,0 +1,13 @@
+package com.maoyou.testseata.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSeataOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSeataOrderApplication.class, args);
+    }
+
+}
---
+++ test-seata-order/src/test/java/com/maoyou/testseata/order/TestSeataOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maoyou.testseata.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSeataOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
